93ea1f345c76d4b5fe12dbf619b8711b10bbda98
kanchangitkothar <kothark0235@gmail.com>
2024-06-10 16:54:44 +0530

    Home-component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,28 +6,5 @@ import { Component, ElementRef, HostListener, QueryList, Renderer2, ViewChildren
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  @ViewChildren('scrollAnimation') scrollElements!: QueryList<ElementRef>;
 
-  ngAfterViewInit() {
-    const options = {
-      root: null, // Use the viewport as the root
-      rootMargin: '0px',
-      threshold: 0.1 // Trigger when 10% of the element is visible
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('fadeInDown');
-          entry.target.classList.remove('invisible');
-          observer.unobserve(entry.target); // Stop observing once the animation is triggered
-        }
-      });
-    }, options);
-
-    this.scrollElements.forEach(element => {
-      element.nativeElement.classList.add('invisible');
-      observer.observe(element.nativeElement);
-    });
-  }
 }
