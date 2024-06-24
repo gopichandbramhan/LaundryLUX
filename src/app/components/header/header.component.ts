ff6475aa06158794deef2e8af18e189eac0dd1cc
Sakshi nikhade <sakshinikhade1318@gmail.com>
2024-06-24 16:18:08 +0530

    contactus page done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+ 
   styleUrls: ['./header.component.css']
+  // styles:['h1{color:red}'],
 })
 export class HeaderComponent {
+  inr: number = 1000; //
 
 }
