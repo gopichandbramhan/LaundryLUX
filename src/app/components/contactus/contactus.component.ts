200f65eb8e4a38e445718974d458e7204bb4db1b
kanchangitkothar <kothark0235@gmail.com>
2024-06-27 11:38:19 +0530

    homecomponent

--- src/app/components/contactus/contactus.component.ts
+++ src/app/components/contactus/contactus.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent {
-
+  resetForm(form: any) {
+    Swal.fire( '!', 'Submitted Successfully');
+    form.resetForm();
+   
+  }
 }
